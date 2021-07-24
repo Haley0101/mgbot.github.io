5a94672f34d13ecb442758aceb7e73f9c2897cac
lavi27 <62757958+lavi27@users.noreply.github.com>
2021-07-24 23:41:42 +0900

    개발자 페이지 수정

--- src/views/Dev.vue
+++ src/views/Dev.vue
@@ -2,81 +2,108 @@
     <div class="content">
         <div class="dev">
             <section class="box1">
-                <div class="content_inner">
-                    <img class="profile" src='https://happycoding.team/static/assets/img/members/Haley.png'>    
-                    <div class="info">
-                        <h1>할리</h1>
-                        <a class="discord_username">할리#0302</a>
-                        <a href="https://github.com/Haley0101"><i class="fab fa-github"></i><!--<img class="icon" src="img/github.svg">--></a>
-                    </div>
-                    <div>
-                        안녕하세요. 미니게임 봇 총괄 디렉터 할리 입니다.
-                    </div>
+                <div class="back_ground">
+                    <img src='img/back_ground/haley.gif'>
+                </div>
+
+                <div class="profile1">
+                    <img class="profile2" src='https://happycoding.team/static/assets/img/members/Haley.png'>    
+                </div>
+
+                <div class="info">
+                    <h1>할리</h1>
+                    <a class="discord_username">할리#0302</a>
+                    <a href="https://github.com/Haley0101"><i class="fab fa-github"></i><!--<img class="icon" src="img/github.svg">--></a>
+                </div>
+
+                <div class="description">
+                    안녕하세요. 미니게임 봇 총괄 디렉터 할리 입니다.
                 </div>
             </section>
         </div>
 
         <div class="dev">
             <section class="box1">
-                <div class="content_inner">
-                    <img class="profile" src='https://happycoding.team/static/assets/img/members/ugi.png'>
-                    <div class="info">
-                        <h1>우기</h1>
-                        <a class="discord_username">우기#0050</a>
-                        <a href="https://github.com/WooGy-U"><i class="fab fa-github"></i><!--<img class="icon" src="img/github.svg">--></a>
-                    </div>
-                    <div>
-                        안녕하세요. 미니게임 봇 디렉터 우기 입니다.
-                    </div>
+                <div class="back_ground">
+                </div>
+
+                <div class="profile1">
+                    <img class="profile2" src='https://happycoding.team/static/assets/img/members/ugi.png'>
+                </div>
+
+                <div class="info">
+                    <h1>우기</h1>
+                    <a class="discord_username">우기#0050</a>
+                    <a href="https://github.com/WooGy-U"><i class="fab fa-github"></i><!--<img class="icon" src="img/github.svg">--></a>
+                </div>
+
+                <div class="description">
+                    안녕하세요. 미니게임 봇 디렉터 우기 입니다.
                 </div>
             </section>
         </div>
 
         <div class="dev">
             <section class="box1">
-                <div class="content_inner">
-                    <img class="profile" src='https://happycoding.team/static/assets/img/members/junyeong.gif'>
-                    <div class="info">
-                        <h1>준영</h1>
-                        <a class="discord_username">준영#7040</a>
-                    </div>
-                    <div>
-                        안녕하세요. 미니게임 봇 디렉터 준영 입니다.
-                    </div>
+                <div class="back_ground">
+                </div>
+
+                <div class="profile1">
+                    <img class="profile2" src='https://happycoding.team/static/assets/img/members/junyeong.gif'>
+                </div>
+
+                <div class="info">
+                    <h1>준영</h1>
+                    <a class="discord_username">준영#7040</a>
+                </div>
+
+                <div class="description">
+                    안녕하세요. 미니게임 봇 디렉터 준영 입니다.
                 </div>
             </section>
         </div>
 
         <div class="dev">
             <section class="box1">
-                <div class="content_inner">
-                    <img class="profile" src='https://happycoding.team/static/assets/img/members/lavi.png'>
-                    <div class="info">
-                        <h1>라비</h1>
-                        <a class="discord_username">라비#2253</a>
-                        <a href="https://github.com/lavi27"><i class="fab fa-github"></i><!--<img class="icon" src="img/github.svg">--></a>
-                        <a href="https://lavi27.kro.kr"><i class="fas fa-globe"></i><!--<img class="icon" src="img/globe.svg">--></a>
-                    </div>
-                    <div>
-                        이 웹사이트를 할리, SSKATE님과 함께 만든 사람입니다.
-                    </div>
+                <div class="back_ground">
+                    <img src='img/back_ground/lavi.png'>
+                </div>
+
+                <div class="profile1">
+                    <img class="profile2" src='https://happycoding.team/static/assets/img/members/lavi.png'>
+                </div>
+
+                <div class="info">
+                    <h1>라비</h1>
+                    <a class="discord_username">라비#2253</a>
+                    <a href="https://github.com/lavi27"><i class="fab fa-github"></i><!--<img class="icon" src="img/github.svg">--></a>
+                    <a href="https://lavi27.kro.kr"><i class="fas fa-globe"></i><!--<img class="icon" src="img/globe.svg">--></a>
+                </div>
+
+                <div class="description">
+                    이 웹사이트를 할리, SSKATE님과<br>함께 만든 사람입니다.
                 </div>
             </section>
         </div>
 
         <div class="dev">
             <section class="box1">
-                <div class="content_inner">
-                    <img class="profile" src='https://happycoding.team/static/assets/img/members/AllPGS.png'>
-                    <div class="info">
-                        <h1>SSKATE</h1>
-                        <a class="discord_username">SSKATE#9781</a>
-                        <a href="https://github.com/kms0219kms"><i class="fab fa-github"></i><!--<img class="icon" src="img/github.svg">--></a>
-                        <a href="https://allpgs.kro.kr"><i class="fas fa-globe"></i><!--<img class="icon" src="img/globe.svg">--></a>
-                    </div>
-                    <div>
-                        이 웹사이트를 할리, 라비님과 함께 만든 사람입니다.
-                    </div>
+                <div class="back_ground">
+                </div>
+
+                <div class="profile1">
+                    <img class="profile2" src='https://happycoding.team/static/assets/img/members/AllPGS.png'>
+                </div>
+
+                <div class="info">
+                    <h1>SSKATE</h1>
+                    <a class="discord_username">SSKATE#9781</a>
+                    <a href="https://github.com/kms0219kms"><i class="fab fa-github"></i><!--<img class="icon" src="img/github.svg">--></a>
+                    <a href="https://allpgs.kro.kr"><i class="fas fa-globe"></i><!--<img class="icon" src="img/globe.svg">--></a>
+                </div>
+
+                <div class="description">
+                    이 웹사이트를 할리, 라비님과 함께 만든 사람입니다.
                 </div>
             </section>
         </div>
