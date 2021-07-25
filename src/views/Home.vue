42329f15607edab569699997488c63dc76bc0237
lavi27 <62757958+lavi27@users.noreply.github.com>
2021-07-26 08:45:37 +0900

    11221212

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,28 +8,21 @@
         <div class="banner_background"><img src="img/banner/banner1.png"></div>
     </div>
 
-<!--<a class="bot_invite" href="https://discord.com/oauth2/authorize?client_id=856090100458979379&permissions=8&scope=bot">봇 초대하기</a>-->
-
     <div class="banner" id="banner2">
-        <h1>환영합니다!</h1>
-        <div>
-            <a class="typing">저희 미니게임 봇 홈페이지에 들러주셔서 감사합니다</a>
-            😍
-        </div>
+        <h1>기능어필</h1>
     </div>
     <div class="banner" id="banner3">
-        <h1>환영합니다!</h1>
+        <h1>MG봇을 사용중인 서버 수</h1>
         <div>
-            <a class="typing">저희 미니게임 봇 홈페이지에 들러주셔서 감사합니다</a>
-            😍
+            <a class="typing">45328754327890543</a>
         </div>
     </div>
     <div class="banner" id="banner4">
-        <h1>환영합니다!</h1>
-        <div>
-            <a class="typing">저희 미니게임 봇 홈페이지에 들러주셔서 감사합니다</a>
-            😍
-        </div>
+        <h1>봇 초대해ㅐㅐㅐㅐㅐㅐㅐㅐ</h1>
+        <a class="bot_invite" href="https://discord.com/oauth2/authorize?client_id=856090100458979379&permissions=8&scope=bot">봇 초대하기</a>
+    </div>
+    <div class="banner" id="banner5">
+        <h1>서포트서버</h1>
     </div>
   </div>
 </template>
