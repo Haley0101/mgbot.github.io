ece3bae6af07b26392104d8b4e8949a50c567e46
lavi27 <62757958+lavi27@users.noreply.github.com>
2021-07-25 19:51:40 +0900

    1

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,11 +4,8 @@
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
     <div class="banner" id="banner1">
         <h1>환영합니다!</h1>
-        <div>
-            <a class="typing">저희 미니게임 봇 홈페이지에 들러주셔서 감사합니다</a>
-            😍
-            <!-- <img src="img/Heart_Eyes_Emoji.png"></img> -->
-        </div>
+        <div class="banner_content"><a class="typing">저희 미니게임 봇 홈페이지에 들러주셔서 감사합니다😍</a></div>
+        <div class="banner_background"><img src="img/banner/banner1.png"></div>
     </div>
     <div class="banner" id="banner2">
         <h1>환영합니다!</h1>
