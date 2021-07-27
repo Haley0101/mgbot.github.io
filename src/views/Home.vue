5f9d7d251669244e96a6c0fdd4653444887c58cb
lavi27 <62757958+lavi27@users.noreply.github.com>
2021-07-27 19:58:40 +0900

    이건 좀 잘만듬

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,49 @@
 <template>
   <div class="home">
     <div class="banner" id="banner1">
-        <div>
-            <h1>환영합니다!</h1>
-            <div class="banner_content"><a class="typing">저희 미니게임 봇 홈페이지에 들러주셔서 감사합니다😍</a></div>
+        <div class="banner_inner">
+            <div class="left_content">
+                <div><h1>환영합니다!</h1></div>
+                <div class="banner_content"><a class="typing">MG봇으로 디스코드 서버 내에서<br>재미있고 다양한 미니게임을 즐겨보세요</a></div>
+                <div class="buttons">
+                    <a class="bot_invite" href="https://discord.com/oauth2/authorize?client_id=856090100458979379&permissions=8&scope=bot">봇 초대하기</a>
+                    <a class="bot_invite" href="https://discord.gg/rRRwEYy8w8">서포트서버</a>
+                </div>
+            </div>
+            <div class="banner_background">
+                <video autoplay muted loop>
+                    <source src="video/banner1.mp4" type="video/mp4"/>
+                </video>
+            </div>
         </div>
-        <div class="banner_background"><img src="img/banner/banner1.png"></div>
     </div>
 
     <div class="banner" id="banner2">
-        <div class="minigame_title">
-            <h1>미니게임</h1>
-            <a>다양하고 재미있는 미니게임을 제공합니다</a>
-        </div>
-        <div class="minigames">
-            <div>efewrf</div>
-            <div>ffewewf</div>
-            <div>eewfewf</div>
-            <div>wefewf</div>
-            <div>efeqwf</div>
-            <div>eqwfqfewfq</div>
+        <div class="banner_inner">
+            <div class="minigame_title">
+                <h1>미니게임</h1>
+                <a>다양하고 재미있는 미니게임을 제공합니다</a>
+            </div>
+            <div class="minigames">
+                <div>efewrf</div>
+                <div>ffewewf</div>
+                <div>eewfewf</div>
+                <div>wefewf</div>
+                <div>efeqwf</div>
+                <div>eqwfqfewfq</div>
+            </div>
         </div>
     </div>
 
     <div class="banner" id="banner3">
         <h1>MG봇을 사용중인 서버 수</h1>
         <div>
-            <a class="typing">45328754327890543</a>
+            <a class="rolling">45328754327890543</a>
         </div>
     </div>
 
     <div class="banner" id="banner4">
         <h1>봇 초대해ㅐㅐㅐㅐㅐㅐㅐㅐ</h1>
-        <a class="bot_invite" href="https://discord.com/oauth2/authorize?client_id=856090100458979379&permissions=8&scope=bot">봇 초대하기</a>
     </div>
 
     <div class="banner" id="banner5">
