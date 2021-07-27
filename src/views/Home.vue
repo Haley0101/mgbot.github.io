f8f409d4843cfa8b70cd3a64b39262393c08ff14
lavi27 <62757958+lavi27@users.noreply.github.com>
2021-07-28 00:21:19 +0900

    12

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,8 +6,8 @@
                 <div><h1>환영합니다!</h1></div>
                 <div class="banner_content"><a class="typing">MG봇으로 디스코드 서버 내에서<br>재미있고 다양한 미니게임을 즐겨보세요</a></div>
                 <div class="buttons">
-                    <a class="bot_invite" href="https://discord.com/oauth2/authorize?client_id=856090100458979379&permissions=8&scope=bot">봇 초대하기</a>
-                    <a class="bot_invite" href="https://discord.gg/rRRwEYy8w8">서포트서버</a>
+                    <a class="button1" href="https://discord.com/oauth2/authorize?client_id=856090100458979379&permissions=8&scope=bot">봇 초대하기</a>
+                    <a class="button1" href="https://discord.gg/uWAnfpXbhd">서포트서버</a>
                 </div>
             </div>
             <div class="banner_background">
@@ -25,29 +25,49 @@
                 <a>다양하고 재미있는 미니게임을 제공합니다</a>
             </div>
             <div class="minigames">
-                <div>efewrf</div>
-                <div>ffewewf</div>
-                <div>eewfewf</div>
-                <div>wefewf</div>
-                <div>efeqwf</div>
-                <div>eqwfqfewfq</div>
+                <div>
+                    <h3>이름 미정</h3>
+                    <a>무기와 갑옷을 강화하는 던전 미니게임</a>
+                </div>
+                <div>
+                    <h3>제작중입니다</h3>
+                    <a>제작중입니다</a>
+                </div>
+                <div>
+                    <h3>제작중입니다</h3>
+                    <a>제작중입니다</a>
+                </div>
+                <div>
+                    <h3>제작중입니다</h3>
+                    <a>제작중입니다</a>
+                </div>
+                <div>
+                    <h3>제작중입니다</h3>
+                    <a>제작중입니다</a>
+                </div>
+                <div>
+                    <h3>제작중입니다</h3>
+                    <a>제작중입니다</a>
+                </div>
             </div>
         </div>
     </div>
 
     <div class="banner" id="banner3">
         <h1>MG봇을 사용중인 서버 수</h1>
-        <div>
-            <a class="rolling">45328754327890543</a>
-        </div>
+        <a class="rolling">0</a>
     </div>
 
     <div class="banner" id="banner4">
-        <h1>봇 초대해ㅐㅐㅐㅐㅐㅐㅐㅐ</h1>
+        <h1>다양한 미니게임을 즐기기 위해<br>
+        봇을 초대해보세요.</h1>
+        <a class="button2" href="https://discord.com/oauth2/authorize?client_id=856090100458979379&permissions=8&scope=bot">봇 초대하기</a>
     </div>
 
     <div class="banner" id="banner5">
-        <h1>서포트서버</h1>
+        <h1>오류가 발생하거나 건의사항 문의시,<br>
+        서포트서버에서 연락주시기 바랍니다.</h1>
+        <a class="button2" href="">서포트서버</a>
     </div>
   </div>
 </template>
