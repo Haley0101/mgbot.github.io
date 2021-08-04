535f109cca3c18ee4c6040411a84205ad16b0be9
lavi27 <62757958+lavi27@users.noreply.github.com>
2021-08-05 01:01:45 +0900

    1

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,11 @@
 <template>
+  <head>
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1.0"
+      user-scaleable="yes"
+    >
+  </head>
   <Header />
   <router-view/>
   <TopButton />
