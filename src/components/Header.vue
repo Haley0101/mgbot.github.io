d089bb24a40e2dfdf0f58d229a05fc78361d00bf
lavi <npt1237@gmail.com>
2021-12-29 02:53:46 +0900

    헤더 모바일 반응형 기초

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -17,7 +17,39 @@
             <div class="RightMenu">
                 <a id="darkBtn"><img style="height: 23px" src="img/light_mode.svg"></a>
                 <a id="lightBtn"><img style="height: 23px" src="img/dark_mode.svg"></a>
+                <div class="burger"><div></div><div></div><div></div></div>
             </div>
         </div>
     </header>
+    <!-- <div class="mobMenu">
+        <div>
+            <router-link to="/">메인</router-link>
+            <router-link to="/path_note">패치 노트</router-link>
+            <router-link to="/help_menu">도움말</router-link>
+            <a href="https://discord.com/oauth2/authorize?client_id=856090100458979379&amp;permissions=8&amp;scope=bot" target="_blank">봇 초대</a>
+            <router-link to="/dev">개발자</router-link>
+        </div>
+        <div class="menuBtns">
+            <a id="darkBtn"><img style="height: 23px" src="img/light_mode.svg"></a>
+            <a id="lightBtn"><img style="height: 23px" src="img/dark_mode.svg"></a>
+        </div>
+    </div> -->
 </template>
+
+<script>
+export default {
+    name: 'App',
+    
+    mounted() {
+        this.btn()
+    },
+    methods: {
+    btn() {
+        const burger = document.getElementsByClassName('burger');
+        burger.addEventListner('click',() => {
+            burger.classList = 'toggle';
+        })
+    }
+    }
+}
+</script>
