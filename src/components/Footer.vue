406e452c31a602cb1b25ad4ed3fedd51fee6f577
lavi <npt1237@gmail.com>
2021-12-30 00:03:05 +0900

    푸터 반응형 추가

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -2,12 +2,14 @@
     <footer>
         <div class="footer3">
             <div class="footer1">
-                <div class="footer-left">
+                <div class="footAbout">
                     <img class="logo4" src="img/logo.svg">
                     <a>미니게임 봇</a>
                 </div>
 
-                <div class="footer-right">
+                <div id="menu">메뉴</div>
+
+                <div class="footMenu">
                     <router-link to="/">메인</router-link>
                     <router-link to="/path_note">패치노트</router-link>
                     <router-link to="/help_menu">도움말</router-link>
@@ -16,17 +18,19 @@
                 </div>
             </div>
             <div class="footer1">
-                <div class="footer-left">
-                    <a>Copyright ⓒ 2021 by HCT</a>
-                </div>
+                <div class="copyright">Copyright ⓒ 2021 by HCT</div>
 
-                <div class="footer-right">
+                <div id="other">등등</div>
+
+                <div class="other">
                     <a href="https://github.com/Happy-Coding-Team">깃허브</a>
                     <a href="https://discord.gg/rRRwEYy8w8">디스코드</a>
                     <a href="https://happycoding.team/">팀사이트</a>
                 </div>
             </div>
         </div>
+
+        <div id="copyright">Copyright ⓒ 2021 by HCT</div>
     </footer>
     
 </template>
