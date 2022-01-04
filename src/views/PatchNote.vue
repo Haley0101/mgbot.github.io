93423c90b4d29df0a019d51cbf09fce49c8169cc
lavi <npt1237@gmail.com>
2022-01-04 23:37:01 +0900

    패치노트 하트 변경

--- src/views/PatchNote.vue
+++ src/views/PatchNote.vue
@@ -4,28 +4,19 @@
             <div class="path_note_header">
                 <div class="user_icon_border"><img class="user_icon" src="https://happycoding.team/static/assets/img/members/Haley.png"></div>
                 <a class="user_name">haley</a>
-                <a class="version">v1.1</a>
+                <a class="version">v1.0</a>
             </div>
             <hr>
             <div class="path_note_content">
                 패치내용. 쏼라쏼라쏼라쏼라쏼라<br>
-                쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라<br>
-                쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라<br>
-                쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라<br>
-                쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라<br>
-                쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라<br>
-                쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라<br>
-                쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라<br>
-                쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라<br>
-                쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라<br>
+                쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라
             </div>
             <hr>
             <div class="path_note_footer">
-                <img class="like" src="img/heart1.svg">
+                <i class="far fa-heart like"></i>
                 <a class="like_count">0</a>
                 <a>2021.1.1</a>
             </div>
         </section>
     </div>
-    
 </template>
