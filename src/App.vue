122f6380da957edec99ab0aaa264b0a976b2c0a0
lavi <npt1237@gmail.com>
2022-01-05 13:56:16 +0900

    탑 버튼 적용 포기

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,21 @@
 <template>
-  <head>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" user-scaleable="yes">
-  </head>
+  <head><meta name="viewport" content="width=device-width, initial-scale=1.0" user-scaleable="yes"></head>
   <Header/>
   <router-view/>
   <Footer/>
 </template>
 
+<style src="../public/css/crossbrowser.css"></style>
+<style src="../public/css/main.css"></style>
+
 <script>
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
-import TopButton from '@/components/top-button';
 
 export default {
   components: {
     Header,
-    Footer,
-    TopButton
+    Footer
   },
   
   mounted () {
--- src/components/top-button.vue
+++
@@ -1,6 +0,0 @@
-<template>
-    <a href="#" class="btn_gotop">
-        <span class="glyphicon glyphicon-chevron-up">
-        </span>
-    </a>
-</template>
