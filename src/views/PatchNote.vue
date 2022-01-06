de864d2d2db83b6c810146d958384779c648f7cb
lavi <npt1237@gmail.com>
2022-01-06 11:47:56 +0900

    패치노트 전체적인 태그, 클래스 스타일 등등 정리

--- src/views/PatchNote.vue
+++ src/views/PatchNote.vue
@@ -1,24 +1,24 @@
 <template>
-    <div class="content">
-        <section class="box1">
-            <div class="path_note_header">
-                <div class="user_icon_border"><img class="user_icon" src="https://happycoding.team/static/assets/img/members/Haley.png"></div>
-                <a class="user_name">haley</a>
-                <a class="version">v1.0</a>
-            </div>
-            <hr>
-            <div class="path_note_content">
-                패치내용. 쏼라쏼라쏼라쏼라쏼라<br>
-                쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라
-            </div>
-            <hr>
-            <div class="path_note_footer">
-                <i class="far fa-heart like"></i>
-                <a class="like_count">0</a>
-                <a>2021.1.1</a>
-            </div>
-        </section>
+<main>
+<div class="card">
+    <div class="pathNote_header">
+        <img src="https://happycoding.team/static/assets/img/members/Haley.png">
+        <a class="name">haley</a>
+        <a class="version">v1.0</a>
     </div>
+    <hr>
+    <p>
+        패치내용. 쏼라쏼라쏼라쏼라쏼라<br>
+        쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라
+    </p>
+    <hr>
+    <div class="pathNote_footer">
+        <i class="far fa-heart like"></i>
+        <a class="like_count">0</a>
+        <a>2021.1.1</a>
+    </div>
+</div>
+</main>
 </template>
 
 <style scoped src="../../public/css/page/patchNote.css"></style>
