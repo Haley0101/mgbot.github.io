dea01a285617f71c921921a6d7343026c9e44d1a
lavi <npt1237@gmail.com>
2022-01-08 04:32:57 +0900

    푸터 태그, 클래스. 스타일 최적화

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,37 +1,37 @@
 <template>
-    <footer>
-        <div class="footer3">
-            <div class="footer1">
-                <div class="footAbout">
-                    <img class="logo4" src="img/logo.svg">
-                    <a>미니게임 봇</a>
-                </div>
+<footer>
+<div class="footer2">
+<div class="footer1">
+    <div class="foot_about">
+        <svg width="1176" height="560" viewBox="0 0 311.14998 148.16667" shape-rendering="crispEdges" xmlns="http://www.w3.org/2000/svg"><g fill="#fff"><path d="M.002 0h148.165v148.165h-42.334v-74.083h-10.584v74.083h-42.334l-.006-74.499-10.578.418v74.083h-42.334z"/><path d="M92.354 10.849a37.086 37.042 0 0 0 0 52.385 37.086 37.042 0 0 0 52.447 0c.95-.949 1.83-1.969 2.642-3.046a37.086 37.042 0 0 0 0-46.292c-.813-1.077-1.692-2.097-2.642-3.046a37.086 37.042 0 0 0-52.447 0zm13.112 13.096a18.543 18.521 0 0 1 26.224 0l-13.112 13.096 13.112 13.096a18.543 18.521 0 0 1-26.224 0 18.543 18.521 0 0 1 0-26.192z" transform="matrix(2 0 0 2 .002 0)"/></g></svg>
+        <a>미니게임 봇</a>
+    </div>
 
-                <div id="menu">메뉴</div>
+    <div class="mobileMenu">메뉴</div>
 
-                <div class="footMenu">
-                    <router-link to="/">메인</router-link>
-                    <router-link to="/path_note">패치노트</router-link>
-                    <router-link to="/help_menu">도움말</router-link>
-                    <a href="https://discord.com/oauth2/authorize?client_id=856090100458979379&permissions=8&scope=bot" target="_blank">봇 초대</a>
-                    <router-link to="/dev">개발자</router-link>
-                </div>
-            </div>
-            <div class="footer1">
-                <div class="copyright">Copyright ⓒ 2021 by HCT</div>
+    <div class="footMenu">
+        <router-link to="/">메인</router-link>
+        <router-link to="/path_note">패치노트</router-link>
+        <router-link to="/help_menu">도움말</router-link>
+        <a href="https://discord.com/oauth2/authorize?client_id=856090100458979379&permissions=8&scope=bot" target="_blank">봇 초대</a>
+        <router-link to="/dev">개발자</router-link>
+    </div>
+</div>
+<div class="footer1">
+    <div class="copyright">Copyright ⓒ 2021 by HCT</div>
 
-                <div id="other">등등</div>
+    <div class="mobileMenu">등등</div>
 
-                <div class="other">
-                    <a href="https://github.com/Happy-Coding-Team">깃허브</a>
-                    <a href="https://discord.gg/rRRwEYy8w8">디스코드</a>
-                    <a href="https://happycoding.team/">팀사이트</a>
-                </div>
-            </div>
-        </div>
+    <div class="other">
+        <a href="https://github.com/Happy-Coding-Team">깃허브</a>
+        <a href="https://discord.gg/rRRwEYy8w8">디스코드</a>
+        <a href="https://happycoding.team/">팀사이트</a>
+    </div>
+</div>
+</div>
 
-        <div id="copyright">Copyright ⓒ 2022 by HCT</div>
-    </footer>
+<div id="copyright">Copyright ⓒ 2022 by HCT</div>
+</footer>
 </template>
 
 
