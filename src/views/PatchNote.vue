63ce18a8cde2689930ea5d012d64618a72fada42
lavi <npt1237@gmail.com>
2022-01-09 05:58:43 +0900

    패치노트 네비게이션 바 추가 등

--- src/views/PatchNote.vue
+++ src/views/PatchNote.vue
@@ -1,5 +1,23 @@
 <template>
 <main>
+<div class="miniNav">
+    <p>2021/01/01</p>
+    <div class="miniNav_nav"></div>
+    <select>
+        <option selected disabled>배열</option>
+        <option value="versionest">버전순</option>
+        <option value="newest">최신순</option>
+        <option value="oldest">오래된순</option>
+        <option value="likest">좋아요순</option>
+    </select>
+    <select>
+        <option selected disabled>분류</option>
+        <option value="versionest">메이저</option>
+        <option value="newest">마이너</option>
+        <option value="newest">공지</option>
+    </select>
+</div>
+<div class="container">
 <div class="card">
     <div class="pathNote_header">
         <img src="https://happycoding.team/static/assets/img/members/Haley.png">
@@ -17,6 +35,50 @@
         <a class="like_count">0</a>
         <a>2021.1.1</a>
     </div>
+</div>
+<div class="card">
+    <div class="pathNote_header">
+        <img src="https://happycoding.team/static/assets/img/members/Haley.png">
+        <a class="name">haley</a>
+        <a class="version">v1.0</a>
+    </div>
+    <hr>
+    <p>
+        패치내용. 쏼라쏼라쏼라쏼라쏼라<br>
+        패치내용. 쏼라쏼라쏼라쏼라쏼라<br>
+        패치내용. 쏼라쏼라쏼라쏼라쏼라<br>
+        패치내용. 쏼라쏼라쏼라쏼라쏼라<br>
+        패치내용. 쏼라쏼라쏼라쏼라쏼라<br>
+        패치내용. 쏼라쏼라쏼라쏼라쏼라
+    </p>
+    <hr>
+    <div class="pathNote_footer">
+        <i class="far fa-heart like"></i>
+        <a class="like_count">0</a>
+        <a>2021.1.1</a>
+    </div>
+</div>
+<div class="card">
+    <div class="pathNote_header">
+        <img src="https://happycoding.team/static/assets/img/members/Haley.png">
+        <a class="name">haley</a>
+        <a class="version">v1.0</a>
+    </div>
+    <hr>
+    <p>
+        패치내용. 쏼라쏼라쏼라쏼라쏼라<br>
+        쏼라쏼라쏼라쏼라쏼라쏼라쏼라쏼라<br>
+        패치내용. 쏼라쏼라쏼라쏼라쏼라<br>
+        패치내용. 쏼라쏼라쏼라쏼라쏼라<br>
+        패치내용. 쏼라쏼라쏼라쏼라쏼라
+    </p>
+    <hr>
+    <div class="pathNote_footer">
+        <i class="far fa-heart like"></i>
+        <a class="like_count">0</a>
+        <a>2021.1.1</a>
+    </div>
+</div>
 </div>
 </main>
 </template>
