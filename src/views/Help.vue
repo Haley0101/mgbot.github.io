1d9057ae4b9380a08dfbd2f0e570a3b925b730c4
lavi <npt1237@gmail.com>
2022-01-09 20:31:39 +0900

    도움말 페이지 navBar및 기초제작

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -1,13 +1,35 @@
 <template>
-    <div class="content" id="path_note">
-        <div class="help_text" style="font-size: 28px;">
-            <div style="font-weight: bold; font-size: 30px;">
-                <br> <p class="help_title_text">출석체크 시스템</p>
-            </div>
-
+<main>
+<div class="help_left">
+    <ul>소개</ul>
+    <ul>초대하기</ul>
+    <ul>초기설정</ul>
+    <ul>부가설정</ul>
+    <ul>미니게임</ul>
+    <ul>부가기능</ul>
+</div>
+<div class="help_right">
+    <h1>초대하기</h1>
+    <hr>
+    <div class="content">
+        <p>
             '/출석체크'를 사용하여 상자, 머니, 포인트를 지급 받을 수 있습니다.<br>
-            '/출석체크 목록'
-            <div>안녕</div>
+            '/출석체크 목록'<a href="https://www.naver.com/">링크</a><br>
+        </p>
+        <div class="code">
+            #include 'stdio.h'<br>
+            <br>
+            int main (void) {<br>
+            &nbsp;int val = 0;<br>
+            <br>
+            &nbsp;printf("%d", val)<br>
+            <br>
+            &nbsp;return 0;<br>
+            }
         </div>
     </div>
+</div>
+</main>
 </template>
+
+<style scoped src="../../public/css/page/help.css"></style>
