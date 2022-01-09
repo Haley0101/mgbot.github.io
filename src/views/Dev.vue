32825a8cf15d01f09883115f64d8c059601c61da
lavi <npt1237@gmail.com>
2022-01-09 20:31:14 +0900

    개발자 페이지 태그 수정 등등

--- src/views/Dev.vue
+++ src/views/Dev.vue
@@ -1,112 +1,104 @@
 <template>
 <main>
 <div class="container">
-<div class="dev">
-    <section class="box1">
-        <div class="backGround">
-            <video loop autoplay src="img/background/haley.webm"></video>
-        </div>
+<section>
+    <div class="backGround">
+        <video loop autoplay src="img/background/haley.webm"></video>
+    </div>
 
-        <div class="profile1">
-            <img src='img/haley.webp'>
-        </div>
+    <div class="profile1">
+        <img src='img/haley.webp'>
+    </div>
 
-        <div class="info">
-            <h1 class="name">할리</h1>
-            <a class="discord_username">할리#0302</a>
-        </div>
+    <div class="info">
+        <h1 class="name">할리</h1>
+        <a class="discord_username">할리#0302</a>
+    </div>
 
-        <div class="links">
-            <a href="https://github.com/Haley0101"><i class="fab fa-github"></i></a>
-        </div>
+    <div class="links">
+        <a href="https://github.com/Haley0101"><i class="fab fa-github"></i></a>
+    </div>
 
-        <div class="description">
-            안녕하세요. 미니게임 봇 총괄 디렉터 할리 입니다.
-        </div>
-    </section>
-</div>
+    <div class="description">
+        안녕하세요. 미니게임 봇 총괄 디렉터 할리 입니다.
+    </div>
+</section>
 
-<div class="dev">
-    <section class="box1">
-        <div class="backGround">
-        </div>
+<section>
+    <div class="backGround">
+    </div>
 
-        <div class="profile1">
-            <video loop autoplay src="img/junyeong.webm"></video>
-        </div>
+    <div class="profile1">
+        <video loop autoplay src="img/junyeong.webm"></video>
+    </div>
 
-        <div class="info">
-            <h1 class="name">준영</h1>
-            <a class="discord_username">준영#7040</a>
-        </div>
+    <div class="info">
+        <h1 class="name">준영</h1>
+        <a class="discord_username">준영#7040</a>
+    </div>
 
-        <div class="links">-</div>
+    <div class="links">-</div>
 
-        <div class="description">
-            안녕하세요. 미니게임 봇 디렉터 준영 입니다.
-        </div>
-    </section>
-</div>
+    <div class="description">
+        안녕하세요. 미니게임 봇 디렉터 준영 입니다.
+    </div>
+</section>
 </div>
 
 <hr>
 
 <div class="container">
-<div class="dev">
-    <section class="box1">
-        <div class="backGround">
-            <img src='img/background/lavi.webp'>
-        </div>
+<section>
+    <div class="backGround">
+        <img src='img/background/lavi.webp'>
+    </div>
 
-        <div class="profile1">
-            <img src='img/lavi.webp'>
-        </div>
+    <div class="profile1">
+        <img src='img/lavi.webp'>
+    </div>
 
-        <div class="info">
-            <h1 class="name">라비</h1>
-            <a class="discord_username">라비 lavi#2253</a>
-        </div>
+    <div class="info">
+        <h1 class="name">라비</h1>
+        <a class="discord_username">라비 lavi#2253</a>
+    </div>
 
-        <div class="links">
-            <a href="https://github.com/lavi27"><i class="fab fa-github"></i></a>
-            <a href="https://blog.naver.com/npt1237"><svg width="188.752" height="175.072" viewBox="0 0 49.940632 46.321136" xmlns="http://www.w3.org/2000/svg"><g fill="#30b44a"><path d="M10.898 17.092c-.95 0-1.72.771-1.72 1.721 0 .95.77 1.721 1.72 1.721s1.721-.771 1.721-1.721c0-.95-.771-1.721-1.721-1.721"/><path d="M27.884 16.913c-1.023 0-1.853.83-1.853 1.853 0 1.023.83 1.853 1.853 1.853 1.023 0 1.853-.83 1.853-1.853 0-1.024-.83-1.853-1.853-1.853"/><path d="M43.937 23.293s.042 4.29-4.316 4.29h-1.326v-2.78h.8s1.832.168 1.811-2.653c0 0-.358 1.142-2.927 1.142 0 0-3.642-.263-3.642-3.817v-1.347s.084-3.685 4.085-3.979c0 0 1.579-.168 2.548 1.137v-.905h2.969zm-16.034 0c-2.71 0-4.906-2.022-4.906-4.516 0-2.495 2.196-4.517 4.906-4.517 2.71 0 4.906 2.022 4.906 4.517 0 2.494-2.196 4.516-4.906 4.516m-6.938-8.765v8.443h-2.927v-8.359c0-1.137-1.411-1.453-1.411-1.453v-2.948c4.674.189 4.337 4.316 4.337 4.316m-5.264 4.675c-.021 3.97-4.127 4.085-4.127 4.085-1.588 0-2.484-1.053-2.484-1.053v.716h-2.99v-12.338.007c.186.03 2.99-.007 2.99-.007v4.716c.703-1.199 2.737-1.179 2.737-1.179 4.427.413 3.874 5.054 3.874 5.054m27.587-19.203h-36.602c-3.692 0-6.685 2.993-6.685 6.685v21.723c0 3.692 2.993 6.685 6.685 6.685h12.928l4.397 10.359s.303.898 1.035.898c.733 0 1.036-.898 1.036-.898l4.397-10.359h12.809c3.692 0 6.685-2.993 6.685-6.685v-21.723c0-3.692-2.993-6.685-6.685-6.685"/><path d="M39.257 16.955c-1 0-1.811.81-1.811 1.811 0 1 .81 1.811 1.811 1.811 1 0 1.811-.81 1.811-1.811 0-1-.811-1.811-1.811-1.811"/></g></svg></a>
-            <a href="https://lavi27.github.io/l/"><i class="fas fa-globe"></i></a>
-        </div>
+    <div class="links">
+        <a href="https://github.com/lavi27"><i class="fab fa-github"></i></a>
+        <a href="https://blog.naver.com/npt1237"><svg width="188.752" height="175.072" viewBox="0 0 49.940632 46.321136" xmlns="http://www.w3.org/2000/svg"><g fill="#30b44a"><path d="M10.898 17.092c-.95 0-1.72.771-1.72 1.721 0 .95.77 1.721 1.72 1.721s1.721-.771 1.721-1.721c0-.95-.771-1.721-1.721-1.721"/><path d="M27.884 16.913c-1.023 0-1.853.83-1.853 1.853 0 1.023.83 1.853 1.853 1.853 1.023 0 1.853-.83 1.853-1.853 0-1.024-.83-1.853-1.853-1.853"/><path d="M43.937 23.293s.042 4.29-4.316 4.29h-1.326v-2.78h.8s1.832.168 1.811-2.653c0 0-.358 1.142-2.927 1.142 0 0-3.642-.263-3.642-3.817v-1.347s.084-3.685 4.085-3.979c0 0 1.579-.168 2.548 1.137v-.905h2.969zm-16.034 0c-2.71 0-4.906-2.022-4.906-4.516 0-2.495 2.196-4.517 4.906-4.517 2.71 0 4.906 2.022 4.906 4.517 0 2.494-2.196 4.516-4.906 4.516m-6.938-8.765v8.443h-2.927v-8.359c0-1.137-1.411-1.453-1.411-1.453v-2.948c4.674.189 4.337 4.316 4.337 4.316m-5.264 4.675c-.021 3.97-4.127 4.085-4.127 4.085-1.588 0-2.484-1.053-2.484-1.053v.716h-2.99v-12.338.007c.186.03 2.99-.007 2.99-.007v4.716c.703-1.199 2.737-1.179 2.737-1.179 4.427.413 3.874 5.054 3.874 5.054m27.587-19.203h-36.602c-3.692 0-6.685 2.993-6.685 6.685v21.723c0 3.692 2.993 6.685 6.685 6.685h12.928l4.397 10.359s.303.898 1.035.898c.733 0 1.036-.898 1.036-.898l4.397-10.359h12.809c3.692 0 6.685-2.993 6.685-6.685v-21.723c0-3.692-2.993-6.685-6.685-6.685"/><path d="M39.257 16.955c-1 0-1.811.81-1.811 1.811 0 1 .81 1.811 1.811 1.811 1 0 1.811-.81 1.811-1.811 0-1-.811-1.811-1.811-1.811"/></g></svg></a>
+        <a href="https://lavi27.github.io/l/"><i class="fas fa-globe"></i></a>
+    </div>
 
-        <div class="description">
-            웹사이트의 프론트엔드를 담당하였습니다.
-        </div>
-    </section>
-</div>
+    <div class="description">
+        웹사이트의 프론트엔드를 담당하였습니다.
+    </div>
+</section>
 
-<div class="dev">
-    <section class="box1">
-        <div class="backGround">
-            <img src='img/background/sskate.webp'>
-        </div>
+<section>
+    <div class="backGround">
+        <img src='img/background/sskate.webp'>
+    </div>
 
-        <div class="profile1">
-            <video loop autoplay src="img/sskate.webm"></video>
-        </div>
+    <div class="profile1">
+        <video loop autoplay src="img/sskate.webm"></video>
+    </div>
 
-        <div class="info">
-            <h1 class="name">SSKATE</h1>
-            <a class="discord_username">SSKATE#0125</a>
-        </div>
+    <div class="info">
+        <h1 class="name">SSKATE</h1>
+        <a class="discord_username">SSKATE#0125</a>
+    </div>
 
-        <div class="links">
-            <a href="https://github.com/kms0219kms"><i class="fab fa-github"></i></a>
-            <a href="https://sskate.me/"><i class="fas fa-globe"></i></a>
-            <a href="https://twitter.com/kms0219kms"><i class="fab fa-twitter"></i></a>
-            <a href="https://instagram.com/kms0219kms"><i class="fab fa-instagram"></i></a>
-            <a href="https://vki.kr/sskate"><i class="fab fa-youtube"></i></a>
-        </div>
+    <div class="links">
+        <a href="https://github.com/kms0219kms"><i class="fab fa-github"></i></a>
+        <a href="https://sskate.me/"><i class="fas fa-globe"></i></a>
+        <a href="https://twitter.com/kms0219kms"><i class="fab fa-twitter"></i></a>
+        <a href="https://instagram.com/kms0219kms"><i class="fab fa-instagram"></i></a>
+        <a href="https://vki.kr/sskate"><i class="fab fa-youtube"></i></a>
+    </div>
 
-        <div class="description">
-            이 웹사이트를 할리, 라비님과 함께 만든 사람입니다.
-        </div>
-    </section>
-</div>
+    <div class="description">
+        이 웹사이트를 할리, 라비님과 함께 만든 사람입니다.
+    </div>
+</section>
 </div>
 </main>
 </template>
