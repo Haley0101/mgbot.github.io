53d2f14f819163cb08c428964a35e5560283f682
lavi <npt1237@gmail.com>
2022-01-10 12:10:34 +0900

    도움말 페이지 기초제작

--- src/views/Help.vue
+++ src/views/Help.vue
@@ -1,14 +1,32 @@
 <template>
-<main>
-<div class="help_left">
-    <ul>소개</ul>
-    <ul>초대하기</ul>
-    <ul>초기설정</ul>
-    <ul>부가설정</ul>
-    <ul>미니게임</ul>
-    <ul>부가기능</ul>
+<main id="help">
+<div class="leftMenu">
+    <div><p>소개</p></div>
+    <div><p>초대하기</p></div>
+    <div><p>초기설정</p></div>
+    <div>
+        <p>부가설정</p>
+        <ul style="height: 0px">
+            <li>부가설정1</li>
+            <li>부가설정2</li>
+        </ul>
+    </div>
+    <div>
+        <p>미니게임</p>
+        <ul style="height: 0px">
+            <li>미니게임1</li>
+            <li>미니게임2</li>
+        </ul>
+    </div>
+    <div>
+        <p>부가기능</p>
+        <ul style="height: 0px">
+            <li>부가기능1</li>
+            <li>부가기능2</li>
+        </ul>
+    </div>
 </div>
-<div class="help_right">
+<div class="main">
     <h1>초대하기</h1>
     <hr>
     <div class="content">
@@ -33,3 +51,31 @@
 </template>
 
 <style scoped src="../../public/css/page/help.css"></style>
+
+<script scoped>
+export default {
+name: 'App',
+
+mounted() {
+const nav = document.querySelector(".leftMenu").childNodes;
+
+for(var i=0; i<nav.length; i++) {
+    let navBtn = nav[i].childNodes;
+    
+    navBtn[0].addEventListener('click',()=>{
+        if(navBtn.length >= 2) {
+            if(navBtn[1].classList == "menu_toggle") {
+                navBtn[1].style.height = "0rem";
+                navBtn[1].style.margin = "0px 0px 0px 10px";
+                navBtn[1].classList = "";
+            } else {
+                navBtn[1].style.height = navBtn[1].childNodes.length * 2 + "rem";
+                navBtn[1].style.margin = "15px 0px 15px 10px";
+                navBtn[1].classList = "menu_toggle";
+            }
+        }
+    })
+}
+}
+}
+</script>
